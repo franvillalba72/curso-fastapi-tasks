b0e9a835e369d2864550507c931bcc9381687d15
Francisco Villalba Garcia <franvillalba@me.com>
2023-09-02 22:06:06 +0200

    Codigos de estado

--- task.py
+++ task.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Body
+from fastapi import APIRouter, status, HTTPException
 from models import Task
 
 router = APIRouter()
@@ -6,32 +6,46 @@ router = APIRouter()
 task_list = []
 
 
-@router.get('/')
+@router.get('/', status_code=status.HTTP_200_OK)
 def get():
     return {'tasks': task_list}
 
 
-@router.post('/')
+@router.post('/', status_code=status.HTTP_201_CREATED)
 def add(task: Task):
+    # Verificamos que la tarea no existe
+    if task in task_list:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Task {task.name} already exists"
+        )
+        return
+
     task_list.append(task)
-    # task_list.append({
-    #     "task": task,
-    #     "status": StatusType.PENDING
-    # })
     return {'tasks': task_list}
 
 
-@router.put('/')
+@router.put('/', status_code=status.HTTP_200_OK)
 def update(index: int, task: Task):
+    # Verificamos que el índice existe
+    if len(task_list) <= index:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Task ID doesn't exists"
+        )
+
     task_list[index] = task
-    # task_list[index] = {
-    #     "task": task,
-    #     "status": status
-    # }
     return {'tasks': task_list}
 
 
-@router.delete('/')
+@router.delete('/', status_code=status.HTTP_200_OK)
 def delete(index: int):
+    # Verificamos que el índice existe
+    if len(task_list) <= index:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Task ID doesn't exists"
+        )
+
     del task_list[index]
     return {'tasks': task_list}
